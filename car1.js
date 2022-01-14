89956646763b7be1debdf87b22db3bc8370f65d1
Tair123 <79557252+Tair123@users.noreply.github.com>
2022-01-13 23:33:24 -0800

    Add files via upload

---
+++ car1.js
@@ -0,0 +1,9 @@
+const carName = 'Porsche Cayman';
+let maxCarSpeed = '274 kmh';
+let carOwner = 'Tair';
+
+maxCarSpeed = '260 kmh';
+carOwner = 'Vladilen'
+
+console.log('У машины изменилась скорость', maxCarSpeed);
+console.log('У машины сменился водитель', carOwner);
---
+++ index1.js
@@ -0,0 +1,11 @@
+let myName = 'Tair';
+let mySurname = 'Zholdassov';
+let myFavoriteDrink = 'Coke';
+let myFavoriteAnimal = 'Cat';
+let myFavoriteProgrammingLanguage = 'HTML';
+
+console.log(myName);
+console.log(mySurname);
+console.log(myFavoriteAnimal);
+console.log(myFavoriteDrink);
+console.log(myFavoriteProgrammingLanguage);
